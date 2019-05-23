b2de9eadd4fc906b6e0822f7eeb7b1f09f80b95c
Hykilpikonna <admin@moecraft.cc>
2019-05-23 17:50:01 -0400

    [+] Create image displayer

---
+++ ImageDisplayer.java
@@ -0,0 +1,16 @@
+import greenfoot.*;
+
+/**
+ * This class displays an image on the screen. It has no functions.
+ *
+ * @author Team APCSA 2019
+ * @author Yijie Gui
+ * @version 2019-05-23 17:47
+ */
+public class ImageDisplayer extends Actor
+{
+    public ImageDisplayer(GreenfootImage image)
+    {
+        setImage(new GreenfootImage(image));
+    }
+}
