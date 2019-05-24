435753ba80bb3737434914dd4d4f1efa8c0dab64
Hykilpikonna <admin@moecraft.cc>
2019-05-24 19:44:51 -0400

    [+] Create KeyFlash and KeyHit class (TODO)

---
+++ KeyFlash.java
@@ -0,0 +1,16 @@
+import greenfoot.Actor;
+
+/**
+ * TODO: This actor displays a flash after the key is hit.
+ * The algorithm should be exactly the same with Key.
+ *
+ * @author Team APCSA 2019
+ * @author Russell Doucet
+ * @author Maanik George
+ * @author Yijie Gui
+ * @since 2019-05-24 14:12
+ */
+public class KeyFlash extends Actor
+{
+
+}
---
+++ KeyHit.java
@@ -0,0 +1,17 @@
+import greenfoot.Actor;
+
+/**
+ * TODO: A hit animation for the keys.
+ * Do this only after most of the project is finished.
+ * Or do this if there's nothing else you can do.
+ *
+ * TODO: Further explanation and image assets needed.
+ *
+ * @author Team APCSA 2019
+ * @author (No one is assigned yet)
+ * @since 2019-05-24 14:15
+ */
+public class KeyHit extends Actor
+{
+
+}
