a27e9f1cc839aafa232854586c119ff340b4ae94
Hykilpikonna <admin@moecraft.cc>
2019-05-24 19:43:30 -0400

    [+] Create judgement controller class

--- JudgementController.java
+++ JudgementController.java
@@ -5,7 +5,45 @@
  * @author Yijie Gui
  * @since 2019-05-22 21:34
  */
+@SuppressWarnings("WeakerAccess")
 public class JudgementController
 {
+    /** Timings, see calculateTimings() method for more info. */
+    public final int[] timings;
 
+    /**
+     *
+     * @param beatmap Beatmap
+     */
+    public JudgementController(Beatmap beatmap)
+    {
+        timings = calculateTimings(beatmap);
+    }
+
+    /**
+     * Calculate the timing values from a beatmap.
+     * Calculation Reference:
+     * https://www.reddit.com/r/osugame/comments/6phntt/difficulty_settings_table_with_all_values/
+     *
+     * @param beatmap The beatmap.
+     * @return Timings, sorted
+     */
+    private static int[] calculateTimings(Beatmap beatmap)
+    {
+        // Get overall difficulty
+        double overall = Double.parseDouble(beatmap.getProperties().get("OverallDifficulty"));
+        double overallOffset = (overall * 3) + 0.5;
+
+        // Create timings array
+        return new int[]
+        {
+                17, // Max
+                (int) (64 - overallOffset), // Great
+                (int) (97 - overallOffset), // Cool
+                (int) (127 - overallOffset), // Good
+                (int) (151 - overallOffset) // Bad
+                // If larger than Bad, ignore it.
+                // If player didn't hit, Poor.
+        };
+    }
 }
