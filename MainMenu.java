2dfbc38975941ea6ce573277ec2d241c138d0f46
Hykilpikonna <admin@moecraft.cc>
2019-05-25 14:58:46 -0400

    [+] Creat main menu (TODO)

---
+++ MainMenu.java
@@ -0,0 +1,29 @@
+import greenfoot.World;
+
+/**
+ * Main song selection menu.
+ *
+ * @author Team APCSA 2019
+ * @author Andy Ge
+ * @author Andrew Vittiglio
+ * @author Steve Rosario
+ * @author Yijie Gui
+ * @author Russell Doucet
+ * @author William LoGiudice
+ * @author Maanik George
+ * @author Mr. Gilmore (Teacher)
+ * @version 2019-05-24 20:37
+ */
+public class MainMenu extends World
+{
+    /**
+     * Constructor for objects of class MainMenu.
+     */
+    public MainMenu()
+    {
+        super(Constants.WIDTH, Constants.HEIGHT, 1);
+
+        // TODO: Someone implement this please! I'm too lazy
+        // For now, right click on the Launcher class, and launch the game there.
+    }
+}
