b6feeffcf67eb3fa9a75a8cc573c337621352264
Hykilpikonna <admin@moecraft.cc>
2019-05-25 19:54:57 -0400

    [+] Create HitImage (TODO)

---
+++ HitImage.java
@@ -0,0 +1,11 @@
+import greenfoot.Actor;
+
+/**
+ * TODO: This class display the hit value after a hit. Eg. Great, Cool, Good, Bad, etc.
+ *
+ * @author Team APCSA 2019
+ * @since 2019-05-25 19:52
+ */
+public class HitImage extends Actor
+{
+}
