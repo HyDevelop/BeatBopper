d8bdacc6a69b2dd059c25a3e82c4141d8f63a362
Hykilpikonna <admin@moecraft.cc>
2019-05-28 16:22:45 -0400

    [+] Create KeyFlash and KeyHitScore

--- KeyFlash.java
+++ KeyFlash.java
@@ -1,16 +1,89 @@
 import greenfoot.Actor;
+import greenfoot.GreenfootImage;
 
 /**
- * TODO: This actor displays a flash after the key is hit.
+ * This actor displays a flash after the key is hit, covering the note landing path.
  * The algorithm should be exactly the same with Key.
  *
  * @author Team APCSA 2019
- * @author Russell Doucet
- * @author Maanik George
- * @author Yijie Gui
+ * @author Steve
  * @since 2019-05-24 14:12
  */
+@SuppressWarnings("WeakerAccess")
 public class KeyFlash extends Actor
 {
+    /** What column it is in */
+    private final int column;
 
+    /** Transparency (0 = fully transparent, 255 = not transparent) */
+    private int transparency = 255;
+
+    /** Is pressing or not */
+    private boolean isPressing;
+
+    /**
+     * Construct a key flash object
+     *
+     * @param column Column number
+     */
+    public KeyFlash(int column)
+    {
+        this.column = column;
+
+        setImage(new GreenfootImage(Images.KEY_FLASH));
+    }
+
+    /**
+     * Initialize the key. This method is called in KeypressHandler when
+     * the key object is created.
+     */
+    public void init()
+    {
+        // Scale image
+        int keyLen = Constants.GRAPHIC_TOTAL_LENGTH / Constants.KEYS.length;
+        getImage().scale(keyLen, Constants.GRAPHIC_NOTE_LANDING);
+
+        // Initialize position
+        int x = Constants.GRAPHIC_COL_OFFSET + keyLen * column;
+        int y = Constants.GRAPHIC_NOTE_LANDING / 2;
+        setLocation(x, y);
+    }
+
+    /**
+     * Act: Dim it
+     */
+    @Override
+    public void act()
+    {
+        // TODO: If not pressing, reduce the transparency by Constants.GRAPHIC_KEY_FLASH_SPEED ( > 0)
+        if (!isPressing)
+        {
+            transparency -= Constants.GRAPHIC_KEY_FLASH_SPEED;
+            if (transparency < 0) transparency = 0;
+        }
+
+        // TODO: Set the transparency of the image.
+        getImage().setTransparency(transparency);
+    }
+
+    /**
+     * This method is called after a key is pressed.
+     */
+    public void press()
+    {
+        // TODO: Reset transparency to not transparent
+        transparency = 255;
+
+        // TODO: Update state of isPressing
+        isPressing = true;
+    }
+
+    /**
+     * This method is called after a key is released.
+     */
+    public void release()
+    {
+        // TODO: Update state of isPressing
+        isPressing = false;
+    }
 }
---
+++ KeyHitScore.java
@@ -0,0 +1,73 @@
+import greenfoot.Actor;
+import greenfoot.GreenfootImage;
+
+/**
+ * TODO: This class display the hit value after a hit. Eg. Great, Cool, Good, Bad, etc.
+ *
+ * @author Team APCSA 2019
+ * @author Andy Ge
+ * @since 2019-05-25 19:52
+ */
+@SuppressWarnings("WeakerAccess")
+public class KeyHitScore extends Actor
+{
+    /** Transparency (0 = fully transparent, 255 = not transparent) */
+    private int transparency = 255;
+
+    /**
+     * Create object and initialize to no image
+     */
+    public KeyHitScore()
+    {
+        setImage(new GreenfootImage(1, 1));
+    }
+
+    /**
+     * Initialize position. This method is called in KeypressHandler when
+     * the key score object is created.
+     */
+    public void init()
+    {
+        // Loop through all the images
+        for (GreenfootImage scoreImage : Images.KEY_HIT_SCORE)
+        {
+            //TODO: Scale the score image
+        }
+
+        // Initialize position
+        int keyLen = Constants.GRAPHIC_TOTAL_LENGTH / Constants.KEYS.length;
+        int x = Constants.GRAPHIC_COL_OFFSET + Constants.GRAPHIC_TOTAL_LENGTH / 2 - keyLen / 2;
+        System.out.println(x);
+        System.out.println(Images.KEY_HIT_SCORE[0].getWidth());
+        int y = (int) Math.round(Constants.GRAPHIC_NOTE_LANDING * (2.0 / 3.0));
+        setLocation(x, y);
+    }
+
+    /**
+     * Act: Dim it
+     */
+    @Override
+    public void act()
+    {
+        // TODO: Reduce the transparency by Constants.GRAPHIC_KEY_HIT_SCORE_SPEED ( > 0)
+        transparency -= Constants.GRAPHIC_KEY_HIT_SCORE_SPEED;
+        if (transparency < 0) transparency = 0;
+
+        // TODO: Set the transparency of the image to the value in the transparency variable.
+        getImage().setTransparency(transparency);
+    }
+
+    /**
+     * This method is called when the player hit a note.
+     *
+     * @param hitScore Hit score (0 to 5)
+     */
+    public void hit(int hitScore)
+    {
+        // TODO: Set the image to the corresponding hit image (Images.KEY_HIT_SCORE[hitScore])
+        setImage(Images.KEY_HIT_SCORE[hitScore]);
+
+        // TODO: Reset the transparency variable to not transparent
+        transparency = 255;
+    }
+}
