b4eba21569fa1add1a84bf09da8da51881ae456b
Hykilpikonna <admin@moecraft.cc>
2019-05-29 18:16:06 -0400

    [+] Create NumberDisplayer class

---
+++ NumberDisplayer.java
@@ -0,0 +1,19 @@
+import greenfoot.Actor;
+import greenfoot.GreenfootImage;
+
+import java.util.LinkedList;
+
+/**
+ * This class contains the utility methods to render scores. The image in
+ * this class aligns from the right (Drawing items from the right to the
+ * left.
+ *
+ * @author Team APCSA 2019
+ * @author Yijie Gui
+ * @author Andy Ge
+ * @since 2019-05-29 17:06
+ */
+@SuppressWarnings("WeakerAccess")
+public abstract class NumberDisplayer extends Actor
+{
+}
