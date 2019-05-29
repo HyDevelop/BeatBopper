7084217397a072f9cf927bd7e1bb5128c3bbac55
Hykilpikonna <admin@moecraft.cc>
2019-05-28 21:56:18 -0400

    [+] Implement the main menu

--- MainMenu.java
+++ MainMenu.java
@@ -1,4 +1,9 @@
+import greenfoot.Color;
+import greenfoot.GreenfootImage;
 import greenfoot.World;
+
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  * Main song selection menu.
@@ -23,7 +28,62 @@ public class MainMenu extends World
     {
         super(Constants.WIDTH, Constants.HEIGHT, 1);
 
-        // TODO: Someone implement this please! I'm too lazy
-        // For now, right click on the Launcher class, and launch the game there.
+        // Get the beatmap sets.
+        ArrayList<File> beatmapSets = BeatmapReader.listBeatmapSets();
+
+        // Draw the background
+        drawBackground();
+
+        // Put song selection buttons in.
+        putSongSelectionButtons(beatmapSets);
+    }
+
+    /**
+     * Draw the background
+     */
+    private void drawBackground()
+    {
+        // Draw wallpaper
+        {
+            GreenfootImage wallpaper = new GreenfootImage(Images.WALLPAPER);
+            int origWidth = wallpaper.getWidth();
+            int origHeight = wallpaper.getHeight();
+            wallpaper.scale((int) (1.0 * origWidth / origHeight * Constants.HEIGHT), Constants.HEIGHT);
+            getBackground().drawImage(wallpaper, 0, 0);
+        }
+
+        // Darken wallpaper with a black overlay
+        {
+            Images.BLACK.scale(Constants.WIDTH, Constants.HEIGHT);
+            Images.BLACK.setTransparency(Constants.GRAPHIC_WALLPAPER_DARKEN);
+            getBackground().drawImage(Images.BLACK, 0, 0);
+        }
+
+        // Draw "Song Select"
+        {
+            // Get text and make an image
+            GreenfootImage text = new GreenfootImage("Song Select", 20, Color.WHITE, null);
+
+            // Calculate the x and y that the text is centered within a separation space.
+            int x = Constants.WIDTH / 2 - text.getWidth() / 2;
+            int y = (Constants.SELECTION_TOP_PADDING + Constants.SELECTION_MIN_SPACING) / 2 - text.getHeight() / 2;
+
+            // Draw it
+            getBackground().drawImage(text, x, y);
+        }
+    }
+
+    /**
+     * Put the song selection buttons, MainMenuSongCover, in the world.
+     */
+    private void putSongSelectionButtons(ArrayList<File> beatmapSets)
+    {
+        for (int i = 0; i < beatmapSets.size(); i++)
+        {
+            File beatmapSetDirectory = beatmapSets.get(i);
+            MainMenuSongCover songSelect = new MainMenuSongCover(beatmapSetDirectory, i);
+            addObject(songSelect, 0, 0);
+            songSelect.init();
+        }
     }
 }
