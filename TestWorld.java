4c4e121070ed37463aff74434580f0f4e2d80d31
Hykilpikonna <admin@moecraft.cc>
2019-05-30 15:26:44 -0400

    [T] Test score report

--- TestWorld.java
+++ TestWorld.java
@@ -19,12 +19,41 @@ public class TestWorld extends World
         super(Constants.WIDTH, Constants.HEIGHT, 1);
         
         getBackground().setColor(Color.BLACK);
-        getBackground().fill();
+        //getBackground().fill();
         
+        /* Testing number displayer
         NumberDisplayer nd = NumberDisplayer.getTestInstance();
         addObject(nd, 0, 0);
         nd.init(Constants.WIDTH / 2, 0);
         
-        nd.drawNumber(10923.23581, 3);
+        nd.drawNumber(10923.23581, 3);*/
+
+        // Show stage images (This is used to test alignment)
+        {
+            GreenfootImage left = Images.STAGE_LEFT;
+            GreenfootImage right = Images.STAGE_RIGHT;
+
+            // Scale height
+            left.scale(left.getWidth(), Constants.HEIGHT);
+            right.scale(right.getWidth(), Constants.HEIGHT);
+
+            // Calculate x and draw image
+            int x = Constants.GRAPHIC_COL_OFFSET - Constants.GRAPHIC_TOTAL_LENGTH / Constants.NUM_COLS / 2;
+            getBackground().drawImage(left, x - left.getWidth(), 0);
+            getBackground().drawImage(right, x + Constants.GRAPHIC_TOTAL_LENGTH, 0);
+        }
+        
+        /* Testing ScoreReport
+        try
+        {
+            ScoreCounter counter = new Gson().fromJson(new FileReader("./score-counter-object-2019-05-29.json"), ScoreCounter.class);
+            ScoreReport sr = new ScoreReport(counter);
+            addObject(sr, Constants.WIDTH / 2, Constants.HEIGHT / 2);
+            sr.draw();
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        }*/
     }
 }
